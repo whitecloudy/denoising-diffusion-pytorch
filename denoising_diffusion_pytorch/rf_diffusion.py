e36a955cdd050f557197b294c82f35d128101d70
Whitecloudy <alex9395@naver.com>
2025-06-09 21:22:07 +0900

    Implement signal diffusion

--- denoising_diffusion_pytorch/rf_diffusion.py
+++ denoising_diffusion_pytorch/rf_diffusion.py
@@ -697,3 +697,379 @@ class GaussianDiffusion(nn.Module):
         t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
 
         return self.p_losses(img, t, *args, **kwargs)
+
+
+class SignalDiffusion(nn.Module):
+    def __init__(
+        self,
+        model,
+        *,
+        data_shape,
+        timesteps = 1000,
+        sampling_timesteps = None,
+        objective = 'pred_noise',
+        beta_schedule = 'cosine',
+        ddim_sampling_eta = 1.,
+        offset_noise_strength = 0.,
+        min_snr_loss_weight = False,
+        min_snr_gamma = 5,
+        tqdm_disable = False,
+        freq_blur = None,
+    ):
+        super().__init__()
+
+        self.model = model
+        self.data_shape = data_shape
+
+        self.objective = objective
+
+        assert objective in {'pred_noise', 'pred_x0', 'pred_v'}, 'objective must be either pred_noise (predict noise) or pred_x0 (predict image start) or pred_v (predict v [v-parameterization as defined in appendix D of progressive distillation paper, used in imagen-video successfully])'
+
+        if beta_schedule == 'linear':
+            betas = linear_beta_schedule(timesteps)
+        elif beta_schedule == 'cosine':
+            betas = cosine_beta_schedule(timesteps)
+        else:
+            raise ValueError(f'unknown beta schedule {beta_schedule}')
+
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, dim=0)
+        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value = 1.)
+
+        timesteps, = betas.shape
+        self.num_timesteps = int(timesteps)
+        self.tqdm_disable = tqdm_disable
+
+        # sampling related parameters
+
+        self.sampling_timesteps = default(sampling_timesteps, timesteps) # default num sampling timesteps to number of timesteps at training
+
+        assert self.sampling_timesteps <= timesteps
+        self.is_ddim_sampling = self.sampling_timesteps < timesteps
+        self.ddim_sampling_eta = ddim_sampling_eta
+       
+        input_time_dim = data_shape[0]
+
+        freq_blur = torch.tensor(default(freq_blur, torch.zeros(timesteps, dtype=torch.float32)+torch.inf), dtype=torch.float32)
+        freq_blur_bar = torch.cumsum(freq_blur, dim=0)
+
+        freq_kernel = (input_time_dim / freq_blur).unsqueeze(1)
+        freq_kernel_bar = (input_time_dim / freq_blur_bar).unsqueeze(1)
+
+        gaussian_kernel = self.get_kernel(freq_kernel, input_time_dim)
+        gaussian_kernel_bar = self.get_kernel(freq_kernel_bar, input_time_dim)
+
+        gamma_weights = gaussian_kernel * torch.sqrt(alphas).unsqueeze(1)
+        gamma_weights_bar = gaussian_kernel_bar * torch.sqrt(alphas_cumprod).unsqueeze(1)
+        gamma_weights_bar_prev = torch.cat([torch.ones_like(gamma_weights_bar[0]).unsqueeze(0), (gamma_weights_bar[:-1])], dim=0)
+
+        sigma_weights = torch.sqrt(betas)
+        sigma_weights_bar = self.get_sigma_bar_weights(input_dim=input_time_dim, 
+                                                       diffusion_steps=timesteps, 
+                                                       gamma_weights=gamma_weights, 
+                                                       sigma_weights=sigma_weights)
+        sigma_weights_bar_prev = torch.cat([torch.ones_like(sigma_weights_bar[0]).unsqueeze(0), (sigma_weights_bar[:-1])], dim=0)
+        
+        # helper function to register buffer from float64 to float32
+        register_buffer = lambda name, val: self.register_buffer(name, val.to(torch.float32))
+
+        register_buffer('gamma_weights', gamma_weights)
+        register_buffer('gamma_weights_bar', gamma_weights_bar)
+        register_buffer('gamma_weights_bar_prev', gamma_weights_bar_prev)
+
+        register_buffer('sigma_weights', sigma_weights)
+        register_buffer('sigma_weights_bar', sigma_weights_bar)
+        register_buffer('sigma_weights_bar_prev', sigma_weights_bar_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        register_buffer('recip_gamma_weights', 1. / gamma_weights)
+        register_buffer('recip_gamma_weights_bar', 1. / gamma_weights_bar)
+        register_buffer('sigma_weights_bar_recip_gamma_weights_bar', sigma_weights_bar / gamma_weights_bar)
+
+        register_buffer('recip_gamma_weights_bar_sqr_plus_sigma_weights_bar_sqr',
+                        1. / (gamma_weights_bar**2 + sigma_weights_bar**2))
+
+        #TODO : REPLACE
+        # register_buffer('betas', betas)
+        # register_buffer('alphas_cumprod', alphas_cumprod)
+        # register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+
+        #TODO : REPLACE
+        # register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))
+        # register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1. - alphas_cumprod))
+        # register_buffer('log_one_minus_alphas_cumprod', torch.log(1. - alphas_cumprod))
+        # register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1. / alphas_cumprod))
+        # register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_cumprod - 1))
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+
+        posterior_variance = (sigma_weights * sigma_weights_bar_prev / sigma_weights_bar)**2
+
+        # above: equal to 1. / (1. / (1. - alpha_cumprod_tm1) + alpha_t / beta_t)
+
+        register_buffer('posterior_variance', posterior_variance)
+
+        # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+
+        register_buffer('posterior_log_variance_clipped', torch.log(posterior_variance.clamp(min =1e-20)))
+        register_buffer('posterior_mean_coef_x_t', (gamma_weights * sigma_weights_bar_prev**2) / (sigma_weights_bar**2))
+        register_buffer('posterior_mean_coef_x_0', (gamma_weights_bar_prev * sigma_weights**2) / (sigma_weights_bar**2))
+
+        # offset noise strength - 0.1 was claimed ideal
+
+        self.offset_noise_strength = offset_noise_strength
+
+        # loss weight
+
+        snr = alphas_cumprod / (1 - alphas_cumprod)
+
+        maybe_clipped_snr = snr.clone()
+        if min_snr_loss_weight:
+            maybe_clipped_snr.clamp_(max = min_snr_gamma)
+
+        if objective == 'pred_noise':
+            loss_weight = maybe_clipped_snr / snr
+        elif objective == 'pred_x0':
+            loss_weight = maybe_clipped_snr
+        elif objective == 'pred_v':
+            loss_weight = maybe_clipped_snr / (snr + 1)
+
+        register_buffer('loss_weight', loss_weight)
+
+    @torch.inference_mode()
+    @staticmethod
+    def get_kernel(blur_kernel, input_dim):
+        samples = torch.arange(0, input_dim) # [N]
+        gaussian_kernel = torch.exp(-((samples - input_dim // 2)**2) / (2 * blur_kernel))
+        # gaussian_kernel = torch.exp(-((samples - self.input_dim // 2)**2) / (2 * var_kernel)) / torch.sqrt(2 * torch.pi * var_kernel) # G_t, [T, N]
+        # gaussian_kernel = self.input_dim * gaussian_kernel / torch.sum(gaussian_kernel, dim=1, keepdim=True) # Normalized G_t, [T, N]
+        return gaussian_kernel
+
+    @torch.inference_mode()
+    @staticmethod
+    def get_sigma_bar_weights(input_dim: int, diffusion_steps: int, gamma_weights, sigma_weights):
+        noise_weights = []
+        noise_weight_square = torch.zeros(input_dim) # [N]
+
+        for t in range(diffusion_steps):
+            noise_weight_square *= (gamma_weights[t] ** 2)
+            noise_weight_square += (torch.ones(input_dim) * sigma_weights[t] ** 2)
+            noise_weights.append(torch.sqrt(noise_weight_square).clone().detach())
+
+        return torch.stack(noise_weights, dim=0) # [T, N] 
+
+
+    @property
+    def device(self):
+        return self.betas.device
+
+    def predict_start_from_noise(self, x_t, t, noise):
+        return (
+            extract(self.recip_gamma_weights_bar, t, x_t.shape) * x_t -
+            extract(self.sigma_weights_bar_recip_gamma_weights_bar, t, x_t.shape) * noise
+        )
+
+    def predict_noise_from_start(self, x_t, t, x0):
+        return (
+            (x_t - extract(self.gamma_weights_bar, t, x_t.shape) * x0) / \
+            extract(self.sigma_weights_bar, t, x_t.shape)
+        )
+
+    def predict_v(self, x_start, t, noise):
+        return (
+            extract(self.gamma_weights_bar, t, x_start.shape) * noise -
+            extract(self.sigma_weights_bar, t, x_start.shape) * x_start
+        )
+
+    def predict_start_from_v(self, x_t, t, v):
+        return (
+            (extract(self.gamma_weights_bar, t, x_t.shape) * x_t -
+            extract(self.sigma_weights_bar, t, x_t.shape) * v) / 
+            extract(self.recip_gamma_weights_bar_sqr_plus_sigma_weights_bar_sqr, t, x_t.shape)
+        )
+
+    def q_posterior(self, x_start, x_t, t):
+        posterior_mean = (
+            extract(self.posterior_mean_coef_x_t, t, x_t.shape) * x_start +
+            extract(self.posterior_mean_coef_x_0, t, x_t.shape) * x_t
+        )
+        posterior_variance = extract(self.posterior_variance, t, x_t.shape)
+        posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, x_t.shape)
+        return posterior_mean, posterior_variance, posterior_log_variance_clipped
+    
+
+    def model_forward(self, x, t, classes):
+        model_out = self.model.forward(x, t, classes)
+        return model_out
+
+
+    def model_predictions(self, x, t, classes, clip_x_start = False):
+        model_output = self.model_forward(x, t, classes)
+        maybe_clip = partial(torch.clamp, min = -1., max = 1.) if clip_x_start else identity
+
+        if self.objective == 'pred_noise':
+            pred_noise = model_output
+
+            x_start = self.predict_start_from_noise(x, t, model_output)
+            x_start = maybe_clip(x_start)
+
+        elif self.objective == 'pred_x0':
+            x_start = model_output
+            x_start = maybe_clip(x_start)
+            x_start_for_pred_noise = x_start
+            pred_noise = self.predict_noise_from_start(x, t, x_start_for_pred_noise)
+
+        elif self.objective == 'pred_v':
+            v = model_output
+            x_start = self.predict_start_from_v(x, t, v)
+            x_start = maybe_clip(x_start)
+
+            x_start_for_pred_noise = x_start
+
+            pred_noise = self.predict_noise_from_start(x, t, x_start_for_pred_noise)
+
+        return ModelPrediction(pred_noise, x_start)
+
+    def p_mean_variance(self, x, t, classes, clip_denoised = True):
+        preds = self.model_predictions(x, t, classes)
+        x_start = preds.pred_x_start
+
+        if clip_denoised:
+            x_start.clamp_(-1., 1.)
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(x_start = x_start, x_t = x, t = t)
+        return model_mean, posterior_variance, posterior_log_variance, x_start
+
+    @torch.no_grad()
+    def p_sample(self, x, t: int, classes, clip_denoised = True):
+        b, device = x.shape[0], x.device
+        batched_times = torch.full((x.shape[0],), t, device = x.device, dtype = torch.long)
+        model_mean, _, model_log_variance, x_start = self.p_mean_variance(x = x, t = batched_times, classes = classes, clip_denoised = clip_denoised)
+        noise = torch.randn_like(x) if t > 0 else 0. # no noise if t == 0
+        pred_img = model_mean + (0.5 * model_log_variance).exp() * noise
+        return pred_img, x_start
+
+    @torch.no_grad()
+    def p_sample_loop(self, classes, shape):
+        batch, device = shape[0], self.betas.device
+
+        img = torch.randn(shape, device=device)
+
+        x_start = None
+
+        for t in tqdm(reversed(range(0, self.num_timesteps)), desc = 'sampling loop time step', total = self.num_timesteps, disable=self.tqdm_disable):
+            img, x_start = self.p_sample(img, t, classes)
+
+        return img
+
+    #TODO : Need to fix for SignalDiffusion
+    @torch.inference_mode()
+    def ddim_sample(self, classes, shape, clip_denoised = True):
+        assert False, "DDIM sampling is not implemented for SignalDiffusion yet."
+    #     batch, device, total_timesteps, sampling_timesteps, eta, objective = shape[0], self.betas.device, self.num_timesteps, self.sampling_timesteps, self.ddim_sampling_eta, self.objective
+
+    #     times = torch.linspace(-1, total_timesteps - 1, steps=sampling_timesteps + 1)   # [-1, 0, 1, 2, ..., T-1] when sampling_timesteps == total_timesteps
+    #     times = list(reversed(times.int().tolist()))
+    #     time_pairs = list(zip(times[:-1], times[1:])) # [(T-1, T-2), (T-2, T-3), ..., (1, 0), (0, -1)]
+
+    #     img = torch.randn(shape, device = device)
+
+    #     x_start = None
+
+    #     for time, time_next in tqdm(time_pairs, desc = 'sampling loop time step', disable=self.tqdm_disable):
+    #         time_cond = torch.full((batch,), time, device=device, dtype=torch.long)
+    #         pred_noise, x_start, *_ = self.model_predictions(img, time_cond, classes, clip_x_start = clip_denoised)
+
+    #         if time_next < 0:
+    #             img = x_start
+    #             continue
+
+    #         alpha = self.gamma_weights_bar[time]
+    #         alpha_next = self.gamma_weights_bar[time_next]
+
+    #         sigma = eta * ((1 - alpha / alpha_next) * (1 - alpha_next) / (1 - alpha)).sqrt()
+    #         c = (1 - alpha_next - sigma ** 2).sqrt()
+
+    #         noise = torch.randn_like(img)
+
+    #         img = x_start * alpha_next.sqrt() + \
+    #               c * pred_noise + \
+    #               sigma * noise
+
+    #     return img
+
+    @torch.inference_mode()
+    def sample_with_class(self, classes):
+        batch_size, data_shape = classes.shape[0], self.data_shape
+        sample_fn = self.p_sample_loop if not self.is_ddim_sampling else self.ddim_sample
+        return sample_fn(classes, (batch_size, *data_shape))
+
+    # # sample with random classes
+    # @torch.inference_mode()
+    # def sample(self, batch_size = 16, cond_scale = 6., rescaled_phi = 0.7):
+    #     classes = torch.randint(0, self.model.num_classes, (batch_size,), device = self.device)
+    #     return self.sample_with_class(classes, cond_scale, rescaled_phi)
+
+    @torch.inference_mode()
+    def interpolate(self, x1, x2, classes, t = None, lam = 0.5):
+        b, *_, device = *x1.shape, x1.device
+        t = default(t, self.num_timesteps - 1)
+
+        assert x1.shape == x2.shape
+
+        t_batched = torch.stack([torch.tensor(t, device = device)] * b)
+        xt1, xt2 = map(lambda x: self.q_sample(x, t = t_batched), (x1, x2))
+
+        img = (1 - lam) * xt1 + lam * xt2
+
+        for i in tqdm(reversed(range(0, t)), desc = 'interpolation sample time step', total = t, disable=self.tqdm_disable):
+            img, _ = self.p_sample(img, i, classes)
+
+        return img
+
+    @autocast('cuda', enabled = False)
+    def q_sample(self, x_start, t, noise=None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        if self.offset_noise_strength > 0.:
+            offset_noise = torch.randn(x_start.shape[:2], device = self.device)
+            noise += self.offset_noise_strength * rearrange(offset_noise, 'b c -> b c 1 1')
+
+        return (
+            extract(self.gamma_weights_bar, t, x_start.shape) * x_start +
+            extract(self.sigma_weights_bar, t, x_start.shape) * noise
+        )
+
+    def p_losses(self, x_start, t, *, classes, noise = None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        # noise sample
+
+        x = self.q_sample(x_start = x_start, t = t, noise = noise)
+
+        # predict and take gradient step
+        model_out = self.model_forward(x, t, classes)
+        if self.objective == 'pred_noise':
+            target = noise
+        elif self.objective == 'pred_x0':
+            target = x_start
+        elif self.objective == 'pred_v':
+            v = self.predict_v(x_start, t, noise)
+            target = v
+        else:
+            raise ValueError(f'unknown objective {self.objective}')
+        loss = F.mse_loss(model_out, target, reduction = 'none')
+        loss = reduce(loss, 'b ... -> b', 'mean')
+
+        loss = loss * extract(self.loss_weight, t, loss.shape)
+        return loss.mean()
+
+    def forward(self, img, *args, **kwargs):
+        shape, device, data_shape = img.shape, img.device, self.data_shape
+        b = shape[0]
+        assert shape[1] == data_shape[0] and shape[2] == data_shape[1] and shape[3] == data_shape[2], f'height and width of image must be {data_shape}'
+        t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
+
+        return self.p_losses(img, t, *args, **kwargs)
